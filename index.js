e7beebfbee94d404154497a6f8c168ee24dd322d
Jesus Velasco <j3zuzw@gmail.com>
2024-07-19 11:07:29 -0600

    validation caracters

--- index.js
+++ index.js
@@ -1,34 +1,79 @@
-const readline = require('readline');
+const readline = require('readline')
 
 // Crear interfaz de lectura
 const rdln = readline.createInterface({
   input: process.stdin,
   output: process.stdout
-});
+})
 
 // Función para verificar la validez de las coordenadas
 const ValidCoord = (lat, lon) => {
-  // Verificar que no haya comas en las coordenadas
-  if (lat.includes(',') || lon.includes(',')) {
-    return false;
+  // Verifica si las coordenadas contienen caracteres no numéricos o comas
+  if (
+    /[^0-9.\-]/.test(lat) ||
+    /[^0-9.\-]/.test(lon) ||
+    lat.includes(',') ||
+    lon.includes(',')
+  ) {
+    return false
   }
 
-  const latNum = parseFloat(lat);
-  const lonNum = parseFloat(lon);
+  // Convierte las entradas a números
+  const latNum = parseFloat(lat)
+  const lonNum = parseFloat(lon)
 
-  return latNum >= -90 && latNum <= 90 && lonNum >= -180 && lonNum <= 180;
+  // Verifica si la conversión fue exitosa y si las coordenadas están dentro de los rangos válidos
+  return (
+    !isNaN(latNum) &&
+    !isNaN(lonNum) &&
+    latNum >= -90 &&
+    latNum <= 90 &&
+    lonNum >= -180 &&
+    lonNum <= 180
+  )
+}
 
-};
+// Función para solicitar la latitud y la longitud
+const askCoordinates = callback => {
+  let lat, lon
 
-// Solicitar latitud y longitud al usuario
-rdln.question('Ingrese la latitud: ', lat => {
-  rdln.question('Ingrese la longitud: ', lon => {
-    if (ValidCoord(lat, lon)) {
-      console.log(`Las coordenadas (${lat}, ${lon}) son válidas`);
-    } else {
-      console.log(`Las coordenadas (${lat}, ${lon}) no son válidas`);
-    }
-    rdln.close();
-  });
-});
+  const Lat = () => {
+    rdln.question('Ingrese la latitud: ', inputLat => {
+      if (ValidCoord(inputLat, '0')) {
+        lat = inputLat
+        Lon()
+      } else {
+        console.log(
+          `La latitud (${inputLat}) no es válida. Por favor, inténtelo de nuevo.`
+        )
+        Lat()
+      }
+    })
+  }
 
+  const Lon = () => {
+    rdln.question('Ingrese la longitud: ', inputLon => {
+      if (ValidCoord(lat, inputLon)) {
+        lon = inputLon
+        callback(lat, lon)
+      } else {
+        console.log(
+          `La longitud (${inputLon}) no es válida. Por favor, inténtelo de nuevo.`
+        )
+        Lon()
+      }
+    })
+  }
+  Lat()
+}
+
+// Función principal para obtener y validar las coordenadas
+const getValidCoordinates = () => {
+  askCoordinates((lat, lon) => {
+    console.log(`Las coordenadas (${lat}, ${lon}) son válidas`)
+    rdln.close()
+  })
+}
+
+// Iniciar la solicitud de coordenadas
+getValidCoordinates()
